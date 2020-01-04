52a970b8f6fb1432cd410a37db8286368edfc409
napsio <napsio@panbachi.de>
2020-01-04 20:47:19 +0100

    tried to modularize code

--- wordclock/src/grid.cpp
+++ wordclock/src/grid.cpp
@@ -8,14 +8,14 @@
 	#define XQUOTE(x) #x
 #endif
 
+#if 0
 void Grid::setTime(int hour, int minute) {
 	if(GRID_LANGUAGE_STRING == "de_DE") {
-		#include "language/de_DE.h"
 		Grid_de_DE::setTime(hour, minute);
 	} else if(GRID_LANGUAGE_STRING == "de_DE_alt") {
-		#include "language/de_DE_alt.h"
 		Grid_de_DE_alt::setTime(hour, minute);
 	} else {
 		Serial.println(GRID_LANGUAGE_STRING);
 	}
 }
+#endif
--- wordclock/src/grid.h
+++ wordclock/src/grid.h
@@ -1,21 +1,26 @@
 #ifndef WORDCLOCK_GRID_H
 #define WORDCLOCK_GRID_H
-  
+
 #ifndef GRID_LANGUAGE
   #define de_DE
   #define GRID_LANGUAGE_STRING "de_DE"
   #include "language/de_DE.h"
+  typedef class Grid_de_DE Grid;
 #else
   #define QUOTE(x) XQUOTE(x)
   #define XQUOTE(x) #x
   #define INCLUDE_FILE(x) QUOTE(language/x.h)
   #define GRID_LANGUAGE_STRING QUOTE(x)
   #include INCLUDE_FILE(GRID_LANGUAGE)
+  #define YYT(x) typedef class Grid_x Grid;
+  YYT(GRID_LANGUAGE)
 #endif
 
+#if 0
 class Grid {
 	public:
 		static void setTime(int hour, int minute);
 };
+#endif
 
 #endif
